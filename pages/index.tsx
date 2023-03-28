79d0a7bae1efa9a006eac54da9d32dcbc42f6f53
Cameron Peck <cameronpeck317@gmail.com>
2023-03-28 15:44:31 -0400

    testing changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-[100px]">Hello Next.js!</h1>
+      <h1 className="text-[100px]">Hello Next.js! Testing changes!</h1>
     </main>
   );
 }

3bb4b10cf6337acc1ba99e8d8b25e9452170d97d
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-03-28 20:24:19 +0000

    Created components for all planets, tested by rendering all on home page at the same time

---
+++ pages/components/jupiter.tsx
@@ -0,0 +1,13 @@
+export default function Jupiter() {
+  return (
+    <div className="w-[30%] planet jupiter">
+      <div className="flex flex-col items-center w-1/3 pt-28">
+        <div className="w-full flex justify-between">
+          <div className="w-[20px] h-[20px] bg-gray-900 rounded-full" />
+          <div className="w-[20px] h-[20px] bg-gray-900 rounded-full" />
+        </div>
+        <div className="w-[80px] h-[40px] rounded-b-full border-b-4 border-l-4 border-gray-900 border-r-4 mt-4" />
+      </div>
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import Mercury from "./components/mercury";
+import Venus from "./components/venus";
+import Earth from "./components/earth";
+import Mars from "./components/mars";
+import Jupiter from "./components/jupiter";
+import Saturn from "./components/saturn";
+import Uranus from "./components/uranus";
+import Neptune from "./components/neptune";
+import Pluto from "./components/pluto";
 
 export default function Home() {
   return (
     <main>
       <Mercury />
+      <Venus />
+      <Earth />
+      <Mars />
+      <Jupiter />
+      <Saturn />
+      <Uranus />
+      <Neptune />
+      <Pluto />
     </main>
   );
 }

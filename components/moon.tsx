e405a8c743447bd516de87d9c55dba9d87beeece
Brandon Moy <brandon@brandonmoy.com>
2023-03-31 22:05:25 +0000

    Added idle animation to moons to give them slight eye catch

--- components/moon.tsx
+++ components/moon.tsx
@@ -38,7 +38,7 @@ const Moons = (props: MoonProps) => {
           key={moon.moonName}
           className="flex justify-center pt-6 basis-full md:basis-1/3 lg:basis-1/5"
         >
-          <div className="w-2/5 planet moon">
+          <div className="w-2/5 planet moon animate-[idle_10s_linear_infinite]">
             <div className="flex flex-col items-center w-1/4 pt-8">
               <div className="flex justify-between w-full">
                 <div className="w-[5px] aspect-square bg-gray-900 rounded-full" />

018e74f87dfb8d27fd187aaaa88ad1ee0af0b137
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-04-02 17:27:54 +0000

    Minor adjustments to spacing/styling of sun and planet faces

--- components/planets/mars.tsx
+++ components/planets/mars.tsx
@@ -1,14 +1,14 @@
 const Mars = () => {
   return (
     <div
-      className={`w-6 md:w-10 lg:w-16 planet mars animate-[front_22s_linear_infinite]`}
+      className={`w-10 md:w-14 lg:w-20 planet mars animate-[front_22s_linear_infinite]`}
     >
-      <div className="flex flex-col items-center w-1/3 pt-2 md:pt-4 lg:pt-8">
+      <div className="flex flex-col items-center w-1/3 pt-2 md:pt-4">
         <div className="flex justify-between w-full">
           <div className="w-1/6 bg-gray-900 rounded-full aspect-square" />
           <div className="w-1/6 bg-gray-900 rounded-full aspect-square" />
         </div>
-        <div className="w-1/2 aspect-video mt-1 border-b-2 border-l-2 border-r-2 border-gray-900 rounded-b-full lg:border-b-4 lg:border-l-4 lg:border-r-4 lg:mt-4" />
+        <div className="w-1/2 aspect-video mt-1 border-b-2 border-l-2 border-r-2 border-gray-900 rounded-b-full lg:border-b-4 lg:border-l-4 lg:border-r-4 lg:mt-2" />
       </div>
     </div>
   );
